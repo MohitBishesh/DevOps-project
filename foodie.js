cc28bb645a0d509a404648fa94b8d3681e521af2
Mohit Bishesh <74617917+MohitBishesh@users.noreply.github.com>
2023-05-01 18:04:29 +0530

    Update foodie.js

--- foodie.js
+++ foodie.js
@@ -18,3 +18,33 @@ const navToggleFunc = function () {
 
 // shopping cart toggle function
 const cartToggleFunc = function () { shoppingCart.classList.toggle('active') }
+
+
+
+
+// add event on nav-toggle-btn
+navToggleBtn.addEventListener('click', function () {
+
+  // If the shopping-cart has an `active` class, it will be removed.
+  if (shoppingCart.classList.contains('active')) cartToggleFunc();
+
+  navToggleFunc();
+
+});
+
+// add event on cart-toggle-btn
+cartToggleBtn.addEventListener('click', function () {
+
+  // If the navbar-nav has an `active` class, it will be removed.
+  if (nav.classList.contains('active')) navToggleFunc();
+
+  cartToggleFunc();
+
+});
+
+// add event on all nav-link
+for (let i = 0; i < navLinks.length; i++) {
+
+  navLinks[i].addEventListener('click', navToggleFunc);
+
+}
