393ee5bef66466dc3606bc170687b2b9499c9fc6
AshutoshL27 <107887875+AshutoshL27@users.noreply.github.com>
2023-05-04 04:25:04 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const http =require('http');
+const fs=require('fs')
+const fileContent =fs.readFileSync('index.html')
+
+const hostname='localhost';
+const port =3000;
+
+const server = http.createServer ((req, res) => {
+  res.writeHead (200,{'Content-Type':'text/html'});
+  res.end( fileContent);
+});
+
+server.listen(port,hostname,()=>{
+    console.log("server running:3000")
+});
